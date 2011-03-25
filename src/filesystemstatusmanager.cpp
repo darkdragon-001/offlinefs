b65b580041f2ca7a0481db324b04bf04660ec71a
Peter Trommler <ptrommler@acm.org>
2011-03-25 10:59:55 +0000

    Replace shell to umount command by umount2 system call if available. Add appropriate checks to build system.

--- src/filesystemstatusmanager.cpp
+++ src/filesystemstatusmanager.cpp
@@ -37,6 +37,9 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#if HAVE_SYS_MOUNT_H
+#include <sys/mount.h>
+#endif /* HAVE_UMOUNT2 */
 using namespace std;
 
 #include "filesystemstatusmanager.h"
@@ -52,10 +55,10 @@ FilesystemStatusManager::FilesystemStatusManager() : available(true) {}
 FilesystemStatusManager::~FilesystemStatusManager(){}
 FilesystemStatusManager& FilesystemStatusManager::Instance()
 {
-    MutexLocker obtain_lock(m);
-    if (theFilesystemStatusManagerInstance.get() == 0)
-      theFilesystemStatusManagerInstance.reset(new FilesystemStatusManager);
-    return *theFilesystemStatusManagerInstance;
+	MutexLocker obtain_lock(m);
+	if (theFilesystemStatusManagerInstance.get() == 0)
+		theFilesystemStatusManagerInstance.reset(new FilesystemStatusManager);
+	return *theFilesystemStatusManagerInstance;
 }
 
 
@@ -96,9 +99,9 @@ void *FilesystemStatusManager::DbusListenerRun(void *)
 	DBusError error;
 	DBusMessage *msg;
 	string filter_string;
-        list<string> interfacelist;
-        const char* device_obj;
-        OFSEnvironment &ofeitp = OFSEnvironment::Instance();
+	list<string> interfacelist;
+	const char* device_obj;
+	OFSEnvironment &ofeitp = OFSEnvironment::Instance();
 	interfacelist=ofeitp.getListenDevices();
 	dbus_error_init (&error);
 	bus = dbus_bus_get (DBUS_BUS_SYSTEM, &error);
@@ -109,34 +112,30 @@ void *FilesystemStatusManager::DbusListenerRun(void *)
 		return NULL;
 	}
 	filter_string=string("type='signal',path=/org/freedesktop/NetworkManager,interface=org.freedesktop.NetworkManager,member=DeviceNoLongerActive");
-	
+
 	dbus_bus_add_match (bus, filter_string.c_str(), NULL);
 	while (1) {
 		dbus_error_init (&error);
-        	dbus_connection_read_write(bus, -1);
-        	msg = dbus_connection_pop_message(bus);
+		dbus_connection_read_write(bus, -1);
+		msg = dbus_connection_pop_message(bus);
 
-      
-      		if (NULL == msg) { 
-         		sleep(1);
-         		continue;
-      		}
+		if (NULL == msg) {
+			sleep(1);
+			continue;
+		}
 		else{
 			if(dbus_message_is_signal(msg,"org.freedesktop.NetworkManager","DeviceNoLongerActive")){
 				dbus_message_get_args (msg, &error,DBUS_TYPE_OBJECT_PATH, &device_obj,DBUS_TYPE_INVALID);
-                                for (list<string>::iterator it=interfacelist.begin();it!=interfacelist.end();++it){
-				string Netpath= "/org/freedesktop/NetworkManager/Devices/"+ (*it);
-                                if(Netpath==device_obj){
-					FilesystemStatusManager::Instance().available=false;
+				for (list<string>::iterator it=interfacelist.begin();it!=interfacelist.end();++it){
+					string Netpath= "/org/freedesktop/NetworkManager/Devices/"+ (*it);
+					if(Netpath==device_obj){
+						FilesystemStatusManager::Instance().available=false;
+					}
 				}
-}
-     
-				
-			
+			}
 		}
-}
-    dbus_message_unref(msg);
-   }
+		dbus_message_unref(msg);
+	}
 	pthread_exit(NULL);
 	return NULL;
 }
@@ -157,81 +156,81 @@ string FilesystemStatusManager::getRemote(string path)
  */
 void FilesystemStatusManager::mountfs()
 {
-    string shareurl = OFSEnvironment::Instance().getShareURL();
-    ofslog::debug("shareURL: %s", shareurl.c_str());
-    string sharepath;
-    string remotefstype;
-    string shareremote;
-    string remotemountpoint;
-    
-    //////////////////////////////////////////////////////////////////////////
-    // MOUNT
-    //////////////////////////////////////////////////////////////////////////
+	string shareurl = OFSEnvironment::Instance().getShareURL();
+	ofslog::debug("shareURL: %s", shareurl.c_str());
+	string sharepath;
+	string remotefstype;
+	string shareremote;
+	string remotemountpoint;
 
-    char* pMountArgumente[8];
-    pMountArgumente[0] = new char[6];
-    strncpy(pMountArgumente[0], "mount", 6);
-    pMountArgumente[1] = new char[3];
-    strncpy(pMountArgumente[1], "-t", 3);
-    pMountArgumente[2] = NULL;
-    pMountArgumente[3] = NULL;
-    pMountArgumente[4] = NULL;
-    pMountArgumente[5] = new char[3];
-    strncpy(pMountArgumente[5], "-o", 3);
-    pMountArgumente[6] = NULL;
-    pMountArgumente[7] = NULL; // terminator
+	//////////////////////////////////////////////////////////////////////////
+	// MOUNT
+	//////////////////////////////////////////////////////////////////////////
 
-    // Ermittelt die Remote-Pfade.
-    //cout << argv[0] << endl;
-    const char* cshareurl = shareurl.c_str();
-    const char* pchDoppelPunktPos = strchr(cshareurl, ':');
-    assert(pchDoppelPunktPos != NULL);
-    int nDoppelPunktIndex = int (pchDoppelPunktPos - cshareurl);
+	char* pMountArgumente[8];
+	pMountArgumente[0] = new char[6];
+	strncpy(pMountArgumente[0], "mount", 6);
+	pMountArgumente[1] = new char[3];
+	strncpy(pMountArgumente[1], "-t", 3);
+	pMountArgumente[2] = NULL;
+	pMountArgumente[3] = NULL;
+	pMountArgumente[4] = NULL;
+	pMountArgumente[5] = new char[3];
+	strncpy(pMountArgumente[5], "-o", 3);
+	pMountArgumente[6] = NULL;
+	pMountArgumente[7] = NULL; // terminator
 
-    remotefstype = shareurl.substr(0,nDoppelPunktIndex);
-    sharepath = shareurl.substr(nDoppelPunktIndex+3);
-    // handle special protocols
-    if(remotefstype == "smb" || remotefstype == "smbfs" || remotefstype == "cifs")
-        shareremote = string("//") + sharepath;
-    else if(remotefstype == "file") {
-	shareremote = string("/") + sharepath;
-	remotemountpoint = shareremote;
-    } else
-        shareremote = sharepath;
+	// Ermittelt die Remote-Pfade.
+	//cout << argv[0] << endl;
+	const char* cshareurl = shareurl.c_str();
+	const char* pchDoppelPunktPos = strchr(cshareurl, ':');
+	assert(pchDoppelPunktPos != NULL);
+	int nDoppelPunktIndex = int (pchDoppelPunktPos - cshareurl);
 
-    char* pArgumente[8];
+	remotefstype = shareurl.substr(0,nDoppelPunktIndex);
+	sharepath = shareurl.substr(nDoppelPunktIndex+3);
+	// handle special protocols
+	if(remotefstype == "smb" || remotefstype == "smbfs" || remotefstype == "cifs")
+		shareremote = string("//") + sharepath;
+	else if(remotefstype == "file") {
+		shareremote = string("/") + sharepath;
+		remotemountpoint = shareremote;
+	} else
+		shareremote = sharepath;
 
-    if(remotefstype != "file") {
-    	// Legt das Dateisystem fest.
-    	pMountArgumente[2] = (char*)remotefstype.c_str();
+	char* pArgumente[8];
 
-    	// Oeffnet die Konfigurationsdatei.
-    	OFSConf& conf = OFSConf::Instance();
-    	//conf.ParseFile(); //obsolete
+	if(remotefstype != "file") {
+		// Legt das Dateisystem fest.
+		pMountArgumente[2] = (char*)remotefstype.c_str();
 
-    	// Legt die Server-Share fest.
-    	pMountArgumente[3] = (char*)shareremote.c_str();
-    	remotemountpoint = conf.GetRemotePath()+"/"+ofs_hash(shareurl);
-    	pMountArgumente[4] = (char*)remotemountpoint.c_str();
+		// Oeffnet die Konfigurationsdatei.
+		OFSConf& conf = OFSConf::Instance();
+		//conf.ParseFile(); //obsolete
 
-    	//    my_options(argc - 2, &argv[2], &pszOptions);
-    	if(OFSEnvironment::Instance().getMountOptions().length() > 0)
-    	{
-    		pMountArgumente[6] = (char *)OFSEnvironment::Instance().getMountOptions().c_str();
-    	}
-    	else
-    	{
-    		pMountArgumente[5] = NULL;
-    	}
-    	// filesystem options
-	//    pArgumente[2] = szOptions;
-    
-    	// Mountet die Share, die vom Benutzer übergeben wurde.
-    	int childpid = fork();
-    	int status;
-        ofslog::info("Mounting filesystem");
-    	if(childpid == 0) {
-    		/*cout << endl << "mount" << " ";
+		// Legt die Server-Share fest.
+		pMountArgumente[3] = (char*)shareremote.c_str();
+		remotemountpoint = conf.GetRemotePath()+"/"+ofs_hash(shareurl);
+		pMountArgumente[4] = (char*)remotemountpoint.c_str();
+
+		//    my_options(argc - 2, &argv[2], &pszOptions);
+		if(OFSEnvironment::Instance().getMountOptions().length() > 0)
+		{
+			pMountArgumente[6] = (char *)OFSEnvironment::Instance().getMountOptions().c_str();
+		}
+		else
+		{
+			pMountArgumente[5] = NULL;
+		}
+		// filesystem options
+		//    pArgumente[2] = szOptions;
+
+		// Mountet die Share, die vom Benutzer übergeben wurde.
+		int childpid = fork();
+		int status;
+		ofslog::info("Mounting filesystem");
+		if(childpid == 0) {
+			/*cout << endl << "mount" << " ";
 		cout << pMountArgumente[1] << " ";
     		cout << pMountArgumente[2] << " ";
     		cout << pMountArgumente[3] << " ";
@@ -239,34 +238,34 @@ void FilesystemStatusManager::mountfs()
     		cout << pMountArgumente[5] << " ";
     		cout << pMountArgumente[6] << " ";
     		cout << endl;*/
-    		// make the mount point and ignore errors of it does exits
-       		//ofslog::debug("mkdir %s", pMountArgumente[4]);
-       		mkdir(pMountArgumente[4], 0777);
-       		/*ofslog::debug("mount %s %s %s %s %s %s %s",
+			// make the mount point and ignore errors of it does exits
+			//ofslog::debug("mkdir %s", pMountArgumente[4]);
+			mkdir(pMountArgumente[4], 0777);
+			/*ofslog::debug("mount %s %s %s %s %s %s %s",
        				pMountArgumente[0], pMountArgumente[1], pMountArgumente[2],
        				pMountArgumente[3], pMountArgumente[4], pMountArgumente[5],
        				pMountArgumente[6]);*/
-       		execvp("mount", pMountArgumente);
-       		ofslog::error(strerror(errno));
-       		return;
-    	}
-    	if(childpid < 0) {
-    		ofslog::error(strerror(errno));
-        	return;
-    	}
-    	//waitpid(childpid, &status, 0);
-    	int childpid2 = wait(&status);
-	//    cout << status << " - (" << childpid << "/" << childpid2 << ") - " << WEXITSTATUS(status) << endl;
-    	int exitstatus = WEXITSTATUS(status);
-    	ofslog::debug("Mount finished");
-    	if(WIFEXITED(status) && exitstatus) {
-       		errno = exitstatus;
-    		ofslog::error("Unable to mount the remote filesystem!");
-    		ofslog::error(strerror(exitstatus));
-       		return;
-    	}
-    
-    }
+			execvp("mount", pMountArgumente);
+			ofslog::error(strerror(errno));
+			return;
+		}
+		if(childpid < 0) {
+			ofslog::error(strerror(errno));
+			return;
+		}
+		//waitpid(childpid, &status, 0);
+		int childpid2 = wait(&status);
+		//    cout << status << " - (" << childpid << "/" << childpid2 << ") - " << WEXITSTATUS(status) << endl;
+		int exitstatus = WEXITSTATUS(status);
+		ofslog::debug("Mount finished");
+		if(WIFEXITED(status) && exitstatus) {
+			errno = exitstatus;
+			ofslog::error("Unable to mount the remote filesystem!");
+			ofslog::error(strerror(exitstatus));
+			return;
+		}
+
+	}
 }
 
 
@@ -276,6 +275,21 @@ void FilesystemStatusManager::mountfs()
 void FilesystemStatusManager::unmountfs()
 {	
 	int status;
+
+#if HAVE_UMOUNT2
+	const char *target = OFSEnvironment::Instance().getRemotePath().c_str();
+	status = umount2(target, MNT_FORCE);
+
+	if (status == -1)
+	{
+		ofslog::error("Unable to unmount the remote filesystem!");
+		ofslog::error(strerror(errno));
+	}
+	else
+	{
+		ofslog::debug("Filesystem unmounted");
+	}
+#else
 	char *arguments[4];
 	arguments[0] = new char[7];
 	strncpy(arguments[0], "umount", 7);
@@ -303,24 +317,25 @@ void FilesystemStatusManager::unmountfs()
 	}
 	else
 	{
-	    ofslog::debug("Filesystem unmounted");
+		ofslog::debug("Filesystem unmounted");
 	}
+#endif /* HAVE_UMOUNT2 */
 }
 
 void FilesystemStatusManager::setAvailability(bool value)
 {
-    if(available != value)
-    {
-        available = value;
-        if(available)
-        { // mount share and reintegrate
-            mountfs();
-            SynchronizationManager::Instance().ReintegrateAll(
-                OFSEnvironment::Instance().getShareID().c_str());
-        }
-        else
-        { // unmount fs
-            unmountfs();
-        }
-    }
+	if(available != value)
+	{
+		available = value;
+		if(available)
+		{ // mount share and reintegrate
+			mountfs();
+			SynchronizationManager::Instance().ReintegrateAll(
+					OFSEnvironment::Instance().getShareID().c_str());
+		}
+		else
+		{ // unmount fs
+			unmountfs();
+		}
+	}
 }
